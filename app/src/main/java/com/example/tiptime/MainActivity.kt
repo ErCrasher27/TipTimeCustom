57af662ca9b3a6d85baa5af79221ab6551ef55d0
ErCrasher27 <francescograuso@live.it>
2022-10-13 16:16:30 +0100

    backup

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 
@@ -14,12 +18,27 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             val tip =
                 Tip(
-                    costOfService = binding.costOfService.text.toString(),
+                    costOfService = binding.costOfServiceEditText.text.toString(),
                     howWasService = binding.tipOptions.checkedRadioButtonId,
                     roundUp = binding.roundUpSwitch.isChecked
                 )
             binding.tipResult.text = tip.calculateTip().toString()
         }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
