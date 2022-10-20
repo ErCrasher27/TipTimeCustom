021f2af4918926a2e4186c55664c337fdb040253
Vincenzo <vincenzoangelino46@gmail.com>
2022-10-20 14:05:31 +0200

    popup added
    popup doesnt work

---
+++ app/src/main/java/com/example/tiptime/PopUpWindow.kt
@@ -0,0 +1,11 @@
+package com.example.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PopUpWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pop_up_window)
+    }
+}
